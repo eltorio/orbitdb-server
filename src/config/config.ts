c8f8297a13d8092c9170f8d645c5011432cadb9c
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-06-14 14:05:20 +0200

    Change congig

--- src/config/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-    API_PORT: 3000,
-    API_PORT_TLS:3000 //only one server will run, ie: if (process.env.TLS_KEY !== undefined && process.env.TLS_CERT !== undefined && config.API_PORT_TLS !== undefined) {
-  }
